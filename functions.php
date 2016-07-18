501091afe7c0bc5173b74e922a0518e189a75598
Pradyot Ghate <pradyot.ghate@gmail.com>
2016-07-19 01:34:06 +0530

    fixed the stylesheet enqueue error

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php 
-	wp_enqueue_style('style', get_stylesheet_uri());
+	function enqueue_stylesheet(){
+		wp_enqueue_style('style', get_stylesheet_uri());
+	}
+
+	add_action( 'wp_enqueue_scripts', 'enqueue_stylesheet' );
 
 	add_theme_support('post-thumbnails');
 ?>
