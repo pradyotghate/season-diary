d25ffd87a83361ce1afd6afaf0dd1694df3213e3
Pradyot Ghate <pradyot.ghate@gmail.com>
2016-08-25 17:06:43 +0530

    enabled FontAwesome

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <div class="footer">
-	Powered by <a href="https://wordpress.org">Wordpress</a>. Theme available on <a href="https://github.com/pradyotghate/season-diary">Github</a>.
+	Powered by <a href="https://wordpress.org"><span class="icon">&#xf19a;</span> Wordpress</a>.
+	Theme available on <a href="https://github.com/pradyotghate/season-diary"><span class="icon">&#xf09b;</span> Github</a>.
 	<?php wp_footer(); ?>
 </div>
 	</body>
--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <!-- Search Form -->
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<input type="text" class="search-field" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder', 'twentysixteen' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+		<input type="text" class="search-field" placeholder="Search" value="<?php echo get_search_query(); ?>" name="s" />
 </form>
